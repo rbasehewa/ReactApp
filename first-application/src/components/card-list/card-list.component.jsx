1355fd586f27fa1ee0cd79cb5956e051914b9b5b
Rangana Maddumahewa <rbasehewa@gmail.com>
2019-11-18 10:15:08 +1030

    Adding componet file

--- first-application/src/components/card-list/card-list.component.jsx
+++ first-application/src/components/card-list/card-list.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './card-list.styles.css';
 
-export const CardList = props => {
-
-        return <div className='card-list'>{props.children}</div>;
-}
+export const CardList = props => (
+    <div className='card-list'>
+        {props.projects.map(project => (
+            <h1 key={project.id}>{project.name} </h1>
+        ))}
+    </div>
+);
