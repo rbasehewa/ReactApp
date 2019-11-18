bc64c9b6584dd6db7b24d01fc8506d2ff8072245
Rangana Maddumahewa <rbasehewa@gmail.com>
2019-11-18 11:57:20 +1030

    updated project name props

--- first-application/src/components/card-list/card-list.component.jsx
+++ first-application/src/components/card-list/card-list.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './card-list.styles.css';
+import { Card } from '../card/card.component';
 
 export const CardList = props => (
     <div className='card-list'>
         {props.projects.map(project => (
-            <h1 key={project.id}>{project.name} </h1>
+            <Card key={project.id} project={project} />
         ))}
     </div>
 );
